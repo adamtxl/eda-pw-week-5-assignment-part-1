8037269e23e05577848cccf80b313f78416adca1
Adam Troxell <adamtxl@hotmail.com>
2024-04-15 18:13:53 -0500

    Updated the parameters for me, responded to 2, 3, 4

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -25,14 +25,17 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
+let fullName = me.firstName + ' ' + me.lastName;
 
+console.log('My full name is ', fullName);
 
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
+console.log('First favorite food is: ', me.favThreeFoods[0]);
+console.log('Third favorite food is: ', me.favThreeFoods[2]);
 
 
 /* 4. Change a property of an existing object.
@@ -41,7 +44,9 @@ console.log('A little about me:', me);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
-
+console.log('I have ' , me.shoeCount, ' pairs of shoes.');
+me.shoeCount = 7;
+console.log('Now I have ', me.shoeCount, 'pairs!');
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
